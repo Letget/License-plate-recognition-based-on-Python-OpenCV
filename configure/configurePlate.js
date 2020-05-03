486975e28b9e97a05c8875581b5481286674c110
letgotten <43167938+Letget@users.noreply.github.com>
2020-05-03 13:13:37 +0800

    Add files via upload

---
+++ configure/configurePlate.js
@@ -0,0 +1,52 @@
+{
+	"config":[
+	{
+		"flag": 1,
+
+		"_blur": 216,
+		"blur": 3,
+
+        "_rKernel1": 227,
+		"rKernel1": [20, 20],
+
+		"_rKernel2": 241,
+		"rKernel2": [4, 19],
+
+        "_plateBlur": 438,
+        "plateBlur": 3,
+
+        "_adp": [442, "计算的邻近尺寸", "减去的常量(过小字符中间越黑, 过大外部变白)"],
+        "adp": [5, -3],
+
+		"_c": [452, "x", "y"],
+		"c": [2, 2],
+
+		"col_num_limit":10,
+		"row_num_limit":21
+	},
+	{
+		"flag": 0,
+
+		"_blur": 216,
+		"blur": 3,
+
+        "_rKernel1": 227,
+		"rKernel1": [20, 20],
+
+		"_rKernel2": 241,
+		"rKernel2": [4, 19],
+
+        "_plateBlur": 438,
+        "plateBlur": 3,
+
+        "_adp": [442, "计算的邻近尺寸", "减去的常量(过小字符中间越黑, 过大外部变白)"],
+        "adp": [5, -3],
+
+		"_c": [452, "x", "y"],
+		"c": [2, 2],
+
+		"col_num_limit":10,
+		"row_num_limit":21
+	}
+	]
+}
---
+++ configure/configureSVM.js
@@ -0,0 +1,22 @@
+{
+	"config":[
+	{
+		"flag": 1,
+
+        "_CN": [121,  "C: 越大, 误差要求越严格", "gamma: 越大,支持向量越少"],
+        "_HZ": "https://blog.csdn.net/wusecaiyun/article/details/49681431",
+		"CN": [4, 0.5],
+		"HZ": [4, 1.5],
+		"ZM": [4, 0.5]
+	},
+	{
+		"flag": 0,
+
+        "_CN": [121, "C: 越大, 误差要求越严格", "gamma: 越大,支持向量越少"],
+        "_HZ": "https://blog.csdn.net/wusecaiyun/article/details/49681431",
+		"CN": [4, 0.5],
+		"HZ": [4, 1.5],
+		"ZM": [5, 0.1]
+	}
+	]
+}
